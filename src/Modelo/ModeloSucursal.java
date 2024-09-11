d005749d5da7f0d2175a5936cce52ab8215d8fab
javie <javie@Javier>
2024-09-10 21:00:38 -0600

    se realizaron varios ingresos de clases de la tabla sucursal

---
+++ src/Modelo/ModeloSucursal.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import Vistas.VistaSucursal;
+
+/**
+ *
+ * @author javie
+ */
+public class ModeloSucursal {
+    private int codigo_sucursal;
+    private String departamento;
+    private String region;
+    private String nombre;
+    private VistaSucursal sucursal;
+
+    public int getCodigo_sucursal() {
+        return codigo_sucursal;
+    }
+
+    public void setCodigo_sucursal(int codigo_sucursal) {
+        this.codigo_sucursal = codigo_sucursal;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public VistaSucursal getSucursal() {
+        return sucursal;
+    }
+
+    public void setSucursal(VistaSucursal sucursal) {
+        this.sucursal = sucursal;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
